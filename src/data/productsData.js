5b2a4d945edd4a69d3606ccdf5eda3cc3d511915
KRITARTH94 <KRITARTH94@users.noreply.github.com>
2026-07-11 16:57:54 +0530

    Revert VTOL Heavy image to original photo - remove competitor-branded product photo

--- src/data/productsData.js
+++ src/data/productsData.js
@@ -55,7 +55,7 @@ export const productCategories = [
           { label: 'Take Off', value: 'Vertical' },
           { label: 'Endurance', value: '2 hr' },
         ],
-        image: '/products/vtol-heavy.png'
+        image: 'https://static.wixstatic.com/media/7177f3_ff72c844e5614ef891fd8db034fd6b95~mv2.png'
       }
     ]
   },
